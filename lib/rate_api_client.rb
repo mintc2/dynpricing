694c97ae46ab2a5a995e9b29e5d2006bbffff77d
mintc2 <minto@tuta.com>
2026-07-17 10:38:12 +0900

    copy over the repo

---
+++ lib/rate_api_client.rb
@@ -0,0 +1,19 @@
+class RateApiClient
+  include HTTParty
+  base_uri ENV.fetch('RATE_API_URL', 'http://localhost:8080')
+  headers "Content-Type" => "application/json"
+  headers 'token' => ENV.fetch('RATE_API_TOKEN', '')
+
+  def self.get_rate(period:, hotel:, room:)
+    params = {
+      attributes: [
+        {
+          period: period,
+          hotel: hotel,
+          room: room
+        }
+      ]
+    }.to_json
+    self.post("/pricing", body: params)
+  end
+end
